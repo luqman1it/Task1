66db7c4fed0ba5a383329cc0bc90b7b3f6c12fbb
luqman1it <luqman1it@gmail.com>
2024-03-16 19:36:46 +0300

    change background

--- src/component/Hero/Hero.jsx
+++ src/component/Hero/Hero.jsx
@@ -1,28 +1,28 @@
 import React, { useState, useEffect } from 'react'
 import './HeroStyle.css'
 import Category from '../Category/Category'
-// import bg1 from '../../assets/images/banner-01.jpg'
-// import bg2 from '../../assets/images/banner-02.jpg'
-// import bg3 from '../../assets/images/banner-03.jpg'
+import bg1 from '../../assets/images/banner-01.jpg'
+import bg2 from '../../assets/images/banner-02.jpg'
+import bg3 from '../../assets/images/banner-03.jpg'
 export default function Hero() {
-    // const images = [bg1, bg2, bg3];
-    // const [currentImageIndex, setCurrentImageIndex] = useState(1);
-    // const goToPreviousImage = () => {
-    //     setCurrentImageIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length)
-    // }
-    // const goToNextImage = () => {
-    //     setCurrentImageIndex((nextIndex) => (nextIndex + 1) % images.length)
-    // }
-    // useEffect(() => {
-    //     const intervalId = setInterval(goToNextImage, 3000);
+    const images = [bg1, bg2, bg3];
+    const [currentImageIndex, setCurrentImageIndex] = useState(1);
+    const goToPreviousImage = () => {
+        setCurrentImageIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length)
+    }
+    const goToNextImage = () => {
+        setCurrentImageIndex((nextIndex) => (nextIndex + 1) % images.length)
+    }
+    useEffect(() => {
+        const intervalId = setInterval(goToNextImage, 3000);
 
-    //     return () => clearInterval(intervalId);
-    // }, [images.length]);
+        return () => clearInterval(intervalId);
+    }, [images.length]);
     return (
-        <section className="hero" >
+        <section className="hero" style={{ backgroundImage: `url(${images[currentImageIndex]})` }} >
             <Category city="Toronto" country="Canada" content="hurry! get the best villa for you" />
-            <span className="left">&lt;</span>
-            <span className="right">&gt;</span>
+            <span onClick={goToPreviousImage} className="left">&lt;</span>
+            <span onClick={goToNextImage} className="right">&gt;</span>
         </section>
     )
 }
